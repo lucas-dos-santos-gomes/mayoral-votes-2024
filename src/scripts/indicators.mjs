50cd26a70cecd737d00b5263046de704d466c035
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2024-11-21 06:52:11 -0300

    Vote js functions

---
+++ src/scripts/indicators.mjs
@@ -0,0 +1,39 @@
+export const indicators = {
+  15: {
+    name: 'Ricardo Luis Reis Nunes',
+    shortName: 'Ricardo Nunes',
+    entourage: 'MDB',
+    entourageName: 'Movimento Democrático Brasileiro',
+  },
+  28: {
+    name: 'Pablo Henrique Costa Marçal',
+    shortName: 'Pablo Marçal',
+    entourage: 'PRTB',
+    entourageName: 'Partido Renovador Trabalhista Brasileiro',
+  },
+  30: {
+    name: 'Marina Helena Cunha Pereira Santos',
+    shortName: 'Marina Helena',
+    entourage: 'Partido Novo',
+    entourageName: '',
+  },
+  40: {
+    name: 'Tabata Cláudia Amaral de Pontes',
+    shortName: 'Tabata Amaral',
+    entourage: 'PSB',
+    entourageName: 'Partido Socialista Brasileiro',
+  },
+  45: {
+    name: 'Jose Luiz Datena',
+    shortName: 'Datena',
+    entourage: 'PSDB',
+    entourageName: 'Partido da Social Democracia Brasileira',
+  },
+  50: {
+    name: 'Guilherme Castro Boulos',
+    shortName: 'Guilherme Boulos',
+    entourage: 'PSOL',
+    entourageName: 'Partido Socialismo e Liberdade',
+  },
+};
+
---
+++ src/scripts/vote.js
@@ -0,0 +1,58 @@
+import { indicators } from "./indicators.mjs"
+
+const painelVoting = document.querySelector('.painel_voting');
+const painelEnd = document.querySelector('.painel_end');
+const painelNumber = document.querySelectorAll('.painel_voting_number');
+const indicatorImage = document.querySelector('.painel_voting_image img');
+const numberBtns = document.querySelectorAll('.keyboard_numbers button');
+const indicatorName = document.querySelector('.indicators-name');
+const indicatorEntourage = document.querySelector('.indicators-entourage');
+const whiteBtn = document.querySelector('.white-btn');
+const correctBtn = document.querySelector('.correct-btn');
+const confirmBtn = document.querySelector('.confirm-btn');
+
+let indicatorNumber = '';
+
+const keyAudio = new Audio('../assets/audios/key.wav');
+const confirmAudio = new Audio('../assets/audios/confirm.wav');
+
+numberBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    keyAudio.play();
+    if(indicatorNumber.length < 2) {
+      indicatorNumber += btn.innerHTML;
+      for(let i = 0; i < indicatorNumber.length; i++) {
+        painelNumber[i].innerHTML = indicatorNumber[i];
+      }
+    }
+
+    if(indicatorNumber.length >= 2) {
+      if(indicators[indicatorNumber]) {
+        indicatorName.innerHTML = indicators[indicatorNumber].shortName;
+        indicatorEntourage.innerHTML = indicators[indicatorNumber].entourage;
+
+        indicatorImage.src = `../assets/imgs/${indicators[indicatorNumber].shortName.toLowerCase().replace(' ', '-').replace('ç', 'c')}.jpg`;
+        indicatorImage.classList.remove('none');
+      } else {
+        indicatorName.innerHTML = 'Voto nulo';
+        indicatorEntourage.innerHTML = 'Voto nulo';
+      }
+    }
+  });
+});
+
+whiteBtn.addEventListener('click', () => {
+  confirmAudio.play();
+  painelVoting.classList.add('none');
+  painelEnd.classList.remove('none');
+});
+
+correctBtn.addEventListener('click', () => {
+  keyAudio.play();
+  indicatorNumber = '';
+  painelNumber.forEach(el => el.innerHTML = '');
+  indicatorName.innerHTML = '__________';
+  indicatorEntourage.innerHTML = '__________';
+  indicatorImage.classList.toggle('none', true);
+});
+
