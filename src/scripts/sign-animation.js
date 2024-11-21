c81d6797314c8e6420096f45c1ed0fd80d32664b
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2024-11-20 21:58:56 -0300

    Login page 100% responsive

--- src/scripts/sign-animation.js
+++ src/scripts/sign-animation.js
@@ -1,10 +1,13 @@
 const container = document.getElementById('container');
 const overlayCon = document.getElementById('overlayCon');
 const overlayBtn = document.getElementById('overlayBtn');
+const responsiveBtns = document.querySelectorAll('.responsive-btn');
 
-overlayBtn.addEventListener('click', () => {
+overlayBtn.addEventListener('click', signAnimation);
+responsiveBtns.forEach(element => element.addEventListener('click', signAnimation));
+
+function signAnimation() {
   container.classList.toggle('right-panel-active');
-
   overlayBtn.classList.remove('btnScaled');
   window.requestAnimationFrame(() => overlayBtn.classList.add('btnScaled'));
-});
+}
